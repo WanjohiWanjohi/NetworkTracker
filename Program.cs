15345036c36eee2447054659f1baa9fd20162a02
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2021-07-28 14:53:06 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NetMon_Ex;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using NetMon;
+
+namespace NetMon_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NM_Monitor nmMonitor = new NM_Monitor();
+            NM_Adapter[] arrAdapters = nmMonitor.arrAdapters;
+            if (arrAdapters.Length == 0)
+            {
+                Console.WriteLine("No network adapters found.");
+                return;
+            }
+
+            nmMonitor.Start();
+            for (int i = 0; i >= 0; i++)
+            {
+                foreach (NM_Adapter tmpAdapter in arrAdapters)
+                {
+
+/*                    Console.WriteLine(tmpAdapter.strAdapterName);
+*/
+                    Console.WriteLine("Download: " + tmpAdapter.DownloadSpeedKbps.ToString() + " kbps " + Environment.NewLine +
+                       "Upload: " + tmpAdapter.UploadSpeedKbps + " kbps as at "+ DateTime.Now);
+
+                    System.Threading.Thread.Sleep(10000);
+                }
+
+            }
+
+        
+    }
+    }
+}
