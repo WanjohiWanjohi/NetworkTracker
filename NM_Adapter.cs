cfe9cf60f2fef1d73df0f59870796d9c6187b44d
WanjohiWanjohi <wamuyuwanjohi97@gmail.com>
2021-07-28 14:49:20 +0300

    Add project files.

---
+++ NM_Adapter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+
+using System.Diagnostics;
+namespace NetMon
+{
+    public class NM_Adapter
+    {
+        internal NM_Adapter(string strName)
+        {
+            strAdapterName = strName; 
+        }
+        private long lngDownloadSpeed { get; set; }
+        private long lngUploadSpeed { get; set; }
+        private long lngDownloadValue { get; set; }
+        private long lngUploadValue { get; set; }
+        private long lngOldDownloadValue { get; set; }
+        private long lngOldUploadValue { get; set; }
+        internal string strAdapterName { get { return strAdapterName; } set {} }
+        public long DownloadSpeed { get { return lngDownloadSpeed; } }
+        public long UploadSpeed { get { return lngUploadSpeed; } }
+
+        public double DownloadSpeedKbps { get { return lngDownloadSpeed / 1024.0; } }
+        public double UploadSpeedKbps { get { return lngUploadSpeed / 1024.0; } }
+        internal PerformanceCounter pcDownloadCounter;
+        internal PerformanceCounter pcUploadCounter; 
+
+        internal void Initialize()
+        {
+            lngOldDownloadValue = pcDownloadCounter.NextSample().RawValue;
+            lngOldUploadValue = pcUploadCounter.NextSample().RawValue;
+        }
+        internal void Update()
+        {
+            lngDownloadValue = pcDownloadCounter.NextSample().RawValue;
+            lngUploadValue = pcUploadCounter.NextSample().RawValue;
+
+            lngDownloadSpeed = lngDownloadValue - lngOldDownloadValue;
+            lngUploadSpeed = lngUploadValue - lngOldUploadValue;
+
+            lngOldDownloadValue = lngDownloadValue;
+            lngOldUploadValue = lngUploadValue;
+        }
+
+        public override string ToString()
+        {
+            return this.strAdapterName;
+        }
+    }
+}
---
+++ NM_Monitor.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Timers;
+using System.Collections;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using NetMon;
+
+namespace NetMon_Ex
+{
+
+	public class NM_Monitor
+	{
+		private Timer tmrTime;
+		private ArrayList alAdapters;
+		private ArrayList alAdaptersMonitored;
+
+		public NM_Monitor()
+		{
+			alAdapters = new ArrayList();
+			alAdaptersMonitored = new ArrayList();
+
+			LoopAdapters();
+
+			tmrTime = new Timer(1000);
+			tmrTime.Elapsed += new ElapsedEventHandler(tmrTime_Elapsed);
+		}
+
+		private void LoopAdapters()
+		{
+
+            PerformanceCounterCategory pcNetworkInterface = new PerformanceCounterCategory("Network Interface");
+
+			foreach (string tmpName in pcNetworkInterface.GetInstanceNames())
+			{
+
+				if (tmpName == "MS TCP Loopback interface")
+					continue;
+
+				NM_Adapter netAdapter = new NM_Adapter(tmpName);
+
+				netAdapter.pcDownloadCounter = new PerformanceCounter("Network Interface", "Bytes Received/sec", tmpName);
+				netAdapter.pcUploadCounter = new PerformanceCounter("Network Interface", "Bytes Sent/sec", tmpName);
+
+				alAdapters.Add(netAdapter);
+			}
+		}
+
+		private void tmrTime_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			foreach (NM_Adapter tmpAdapter in alAdaptersMonitored)
+				tmpAdapter.Update();
+		}
+
+		public NM_Adapter[] arrAdapters
+		{
+			get
+			{
+				return (NM_Adapter[])alAdapters.ToArray(typeof(NM_Adapter));
+			}
+		}
+
+		public void Start()
+		{
+			if (this.alAdapters.Count > 0)
+			{
+				foreach (NM_Adapter currAdapter in alAdapters)
+
+					if (!alAdaptersMonitored.Contains(currAdapter))
+					{
+						alAdaptersMonitored.Add(currAdapter);
+						currAdapter.Initialize();
+					}
+
+				tmrTime.Enabled = true;
+			}
+		}
+
+		public void Start(NM_Adapter nmAdapter)
+		{
+			if (!alAdaptersMonitored.Contains(nmAdapter))
+			{
+				alAdaptersMonitored.Add(nmAdapter);
+				nmAdapter.Initialize();
+			}
+
+			tmrTime.Enabled = true;
+		}
+
+		public void Stop()
+		{
+			alAdaptersMonitored.Clear();
+			tmrTime.Enabled = false;
+		}
+
+		public void Stop(NM_Adapter currAdapter)
+		{
+			if (alAdaptersMonitored.Contains(currAdapter))
+				alAdaptersMonitored.Remove(currAdapter);
+
+			if (alAdaptersMonitored.Count == 0)
+				tmrTime.Enabled = false;
+		}
+	}
+}
